8c9a7a90f837d47ff674e8d8c3dcfb7ceffae2cb
Tyrus <tyrus.miles@gmail.com>
2020-09-30 18:49:30 -0700

    made callout a component, and made a /blog index page

--- personal-site/pages/blog/example.js
+++ personal-site/pages/blog/example.js
@@ -2,6 +2,7 @@ import ToggleList from '../../components/blog/toggleList';
 import BlogFooter from '../../components/blog/BlogFooter';
 import CodeBlock from '../../components/blog/CodeBlock'
 import H from '../../components/blog/H'
+import Callout from '../../components/callout'
 import { goals } from '../../external-calls/goals';
 export default function Example(){
 
@@ -16,11 +17,7 @@ export default function Example(){
     ))};`;
     return(
         <>
-            <div className="article-overview flex">
-                <span className="summary-tag">Summary</span>
-                <div>This is a brief overview of my blog layout as well as components for it</div>
-            </div>
-
+            <Callout>This is a brief overview of my blog layout as well as components for it</Callout>
             <div className="article-header">
                 <div className="flex-justify-between">
                     <div className="article-date">March 23, 2020</div>
@@ -37,7 +34,7 @@ export default function Example(){
                 <ToggleList itemList={goals}></ToggleList>
 
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui veniam in reiciendis dolorem molestias quasi voluptatem dolor assumenda velit eligendi vel sit, itaque est illum ullam doloribus non, iure totam modi. Hic, earum nostrum ut illum, dolorem fugit voluptatum rem id incidunt libero ullam minus quidem itaque, sunt nobis? Eum deserunt dolor quibusdam ea quo recusandae dignissimos, nam cum tempore officiis a omnis rem vero eveniet quidem eos dolorum obcaecati quod earum placeat voluptatum. Incidunt laborum provident asperiores? Veritatis ducimus quidem atque dignissimos tenetur rem maiores harum quasi quod recusandae a libero quis officiis at, esse enim totam numquam possimus!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui veniam in reiciendis <H>dolorem molestias quasi voluptatem</H> dolor assumenda velit eligendi vel sit, itaque est illum ullam doloribus non, iure totam modi. Hic, earum nostrum ut illum, dolorem fugit voluptatum rem id incidunt libero ullam minus quidem itaque, sunt nobis? Eum deserunt dolor quibusdam ea quo recusandae dignissimos, nam cum tempore officiis a omnis rem vero eveniet quidem eos dolorum obcaecati quod earum placeat voluptatum. Incidunt laborum provident asperiores? Veritatis ducimus quidem atque dignissimos tenetur rem maiores harum quasi quod recusandae a libero quis officiis at, esse enim totam numquam possimus!
                 </p>
 
                 <p className="code-p">
@@ -66,22 +63,33 @@ export default function Example(){
                 .summary-tag{
                     border-radius: 10px;
                     // background-color: #1976d2;
+                    background-color: #52E5AA;;
+                    color: #3EBA88;
+                    color: #fff;
                     // color: #f2f5fa;
                     height: max-content;
-                    color: black;
-                    background-color: white;
+                    // color: black;
+                    // background-color: white;
                     padding: 5px 10px;
                     margin-right: 20px;
+                    text-decoration: none;
                 }
                 .article-overview{
-                    padding: 20px 50px;
+                    line-height: 1.4;
+                    display: flex;
+                    align-items: center;
+                    padding: 15px 50px;
                     margin-top: 50px;
+                    border: 1px solid whitesmoke;
                     // background-color: #f2f5fa;
-                    background-color: black;
-                    color: white;
+                    background-color: white;
+                    // color: white;
                     border-radius: 10px;
                     margin-bottom: 20px;
-
+                    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+                }
+                .article-overview div{
+                    text-decoration: underline;
                 }
             `}</style>
         </>
--- personal-site/pages/index.js
+++ personal-site/pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
 import Link from 'next/link';
 
 
