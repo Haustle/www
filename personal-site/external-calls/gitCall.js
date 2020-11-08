db83e20ac14f3cb83c56f258c099209c3127a508
Tyrus <tyrus.miles@gmail.com>
2020-11-08 14:54:50 -0700

    fixed github api call to use @octokit/core

--- personal-site/external-calls/gitCall.js
+++ personal-site/external-calls/gitCall.js
@@ -1,8 +1,8 @@
-// find a way to cache
+const { Octokit } = require("@octokit/core");
+
 const repoFilter = new Set();
 repoFilter.add("cse360assignment02");
 
-const fetch = require('node-fetch');
 
 function dayDifference(date1, date2) {
     const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24; 
@@ -25,14 +25,10 @@ function compareDays (a , b){
 export async function getRepos(userName){
 
     var infoList = [];
-    const headers = {
-        "Authorization" : `Token ${process.env.GIT_KEY}`
-    }
-    const repoList = await fetch(`https://api.github.com/users/${userName}/repos`, {
-        "method": "GET",
-        "headers" : headers
-    });
-    const list = await repoList.json();
+    const octokit = new Octokit({ auth: process.env.GIT_KEY })
+    const repoList = await octokit.request(`/users/${userName}/repos`);
+    const list = await repoList["data"];
+
     for(var x = 0; x < list.length; x++){
         
         const repoName = list[x]['name'];
