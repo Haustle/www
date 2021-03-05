6e376d717822a59f895358f6187c2438aec9f1ee
Tyrus <tyrus.miles@gmail.com>
2021-03-04 18:11:19 -0700

    fixed sorting and .mdx metadata reading

---
+++ personal-site/external-calls/getPostList.js
@@ -0,0 +1,56 @@
+// const path = require("path");
+import fs from 'fs'
+import path from 'path'
+import matter from "gray-matter"
+
+export default function getPosts(){
+
+    // get all files in the /posts directory
+    const postDir = path.join(process.cwd(), "posts");
+    const filenames = fs.readdirSync(postDir).filter(file => file.endsWith(".mdx"))
+
+    let postMap = new Map()
+
+    for (var x = 0; x < filenames.length; x++) {
+        const newpath = path.join(postDir, filenames[x]);
+
+        // open and read the mdx file
+        const rawFileSource = fs.readFileSync(newpath);
+
+        // we extract the header and the contents of the article
+        const { content, data } = matter(rawFileSource)
+
+        const fullDate   = new Date(data.date);
+        const year = fullDate.getFullYear();
+        data["year"] = year
+
+        // if the year doesn't exist we make a new key to the map
+        if (!postMap.has(year)) {
+            postMap.set(year, [])
+        }
+
+        // add the post data as an item of the year
+        postMap.get(year).push(data)
+
+    }
+
+    // turn map into an array
+    const retPost = [...postMap].map(([year, posts]) => ({ year, posts }));
+
+    // sorting all the posts from each year
+    for(var x = 0; x < retPost.length; x++){
+        // order the post from most recent to oldest
+        retPost[x].posts.sort(function (a, b) {
+            return new Date(b.date) - new Date(a.date);
+        });
+
+    }
+
+    // order the years
+    retPost.sort(function(a,b){
+        return b.year - a.year;
+    })
+
+    // return the sorted list of posts
+    return retPost
+}
--- personal-site/pages/blog/[slug].js
+++ personal-site/pages/blog/[slug].js
@@ -1,11 +1,8 @@
 import BlogLayout from "../../components/blog/BlogLayout"
 import fs from 'fs'
 import path from 'path'
-import dynamic from 'next/dynamic'
-import { MDXProvider } from "@mdx-js/react";
 import hydrate from 'next-mdx-remote/hydrate'
 import renderToString from 'next-mdx-remote/render-to-string'
-// import {MDX} from "@mdx-js/runtime";
 
 import IMG from '../../components/blog/Img';
 import A from '../../components/A'
@@ -17,7 +14,7 @@ import matter from "gray-matter"
 const components = { IMG, A }
 
 export default function Post({slug , meta, mdx}){
-    const sor = hydrate(mdx, { components })
+    const sor = hydrate(mdx, {components})
     return (
         <BlogLayout meta={meta}>
             {/* <MDx/> */}
@@ -28,15 +25,13 @@ export default function Post({slug , meta, mdx}){
     
 export const getStaticProps = async ({ params }) => {
     const slug = params.slug;
-    console.log(`start: ${slug}`);
+
 
     const newpath = `${path.join(process.cwd(),"posts",slug)}.mdx`
-
     const rawFileSource = fs.readFileSync(newpath);
     const { content, data } = matter(rawFileSource)
     const mdxSource = await renderToString(content)
 
-    console.log(`end: ${slug}`);
     return {
         props: {
             mdx: mdxSource,
