db6f6af0b8b002e509a6e72bc20b8611a671f7b9
Tyrus <tyrus.miles@gmail.com>
2021-04-30 17:43:13 -0700

    more changes for twitter-cards

--- personal-site/components/layout.js
+++ personal-site/components/layout.js
@@ -24,27 +24,20 @@ export default function Layout({ children, category, meta}) {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 {meta ? 
                     (<>
-                    <meta property="og:title" content={meta.title} />
-                    <meta property="og:description" content={meta.description} />
-                    <meta property="og:url" content={`${host}${path}`} />
-                    <meta property="og:image" content={`${host}${path}/card.png?123`} />
-                    <meta property="og:site_name" content="tyrus.im"/>
-                    <meta property="og:locale" content="en_US"/>
-                    <meta property="og:type" content={path.startsWith("/blog") ? "article" : "website"}/>
+                        <meta property="og:title" content={meta.title} />
+                        <meta property="og:description" content={meta.description} />
+                        <meta property="og:url" content={`${host}${path}`} />
+                        <meta property="og:image" content={`${host}${path}/card.png?123`} />
+                        <meta property="og:type" content={path.startsWith("/blog") ? "article" : "website"}/>
 
+                        
+                        {/* Twitter */}
+                        <meta name="twitter:card" content="summary_large_image"/>
+                        <meta name="twitter:site" content="@haustle" />
+                        <meta name="twitter:title" content={meta.title} />
+                        <meta name="twitter:description" content={meta.description}/>
+                        {/* <meta name="twitter:image" content={`${host}${path}/card.png`}/> */}
 
-
-                    <meta itemProp="author" content="Tyrus"/>
-                    
-                    {/* Twitter */}
-                    <meta name="twitter:card" content="summary_large_image"/>
-                    <meta name="twitter:domain" content={host}/>
-                    <meta name="twitter:url" content={`${host}${path}`}/>
-                    <meta name="twitter:title" content={meta.title} />
-                    <meta name="twitter:description" content={meta.description}/>
-                    <meta name="twitter:site" content="@haustle"/>
-                    <meta name="twitter:image" content={`${host}${path}/card.png?123`}/>
-                    <meta name="twitter:creator" content="@haustle"/>
                     </>)
                 : null }
             </Head>
